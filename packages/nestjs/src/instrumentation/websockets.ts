da9afe3e89fa97483b7dfe176b9fc1cef5978a2a
Angel Calderon Mantilla <angelitosystems@gmail.com>
2026-09-09 18:15:23 -0500

    feat: agregar contexto de instrumentación en WebsocketInstrumentation

--- packages/nestjs/src/instrumentation/websockets.ts
+++ packages/nestjs/src/instrumentation/websockets.ts
@@ -17,8 +17,10 @@ export class WebsocketInstrumentation {
   private readonly redactor: Redactor;
   private readonly config: DevToolsConfig;
   private readonly projectId: string;
+  private readonly ctx: InstrumentationContext;
 
   constructor(ctx: InstrumentationContext) {
+    this.ctx = ctx;
     this.config = ctx.config;
     this.projectId = ctx.projectInfo.projectId;
     this.redactor = new Redactor({
